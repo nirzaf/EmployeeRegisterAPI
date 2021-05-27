43ddb839f485ad38b5f64fb126c98ea4a8269aeb
Quadrate Tech Solutions <info@quadrate.lk>
2021-05-27 13:42:15 +0530

    migration db

--- EmployeeRegisterAPI/Migrations/20210527081145_new-mig.Designer.cs
+++ EmployeeRegisterAPI/Migrations/20210527081145_new-mig.Designer.cs
@@ -3,14 +3,16 @@ using EmployeeRegisterAPI.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EmployeeRegisterAPI.Migrations
 {
     [DbContext(typeof(EmployeeDbContext))]
-    partial class EmployeeDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210527081145_new-mig")]
+    partial class newmig
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EmployeeRegisterAPI/Migrations/20210527081145_new-mig.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeRegisterAPI.Migrations
+{
+    public partial class newmig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "EmployeeID",
+                table: "Employees",
+                newName: "EmployeeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "EmployeeId",
+                table: "Employees",
+                newName: "EmployeeID");
+        }
+    }
+}
